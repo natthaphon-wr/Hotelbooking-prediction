8d671a26cde765e350dd199a44ad613acff30958
natthaphon-wr <natthaphon.win@gmail.com>
2024-03-28 19:52:07 +0700

    complete RF

--- hotel_city.R
+++ hotel_city.R
@@ -1,6 +1,7 @@
 source('preprocess.R')
 library(tree)
 library(tidyverse) 
+library(randomForest)
 
 
 # 1. Import data ----
@@ -10,12 +11,14 @@ sapply(city, class)
 summary(city$is_canceled)
 
 
-# 2. Do classification ----
+# 2. Split cross-validation ----
 set.seed(1)
 folds <- createFolds(city$is_canceled, k=10)
+
+
+# 3. Decision Tree ---- 
 DT_result <- data.frame(matrix(ncol=4, nrow=0))
 colnames(DT_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
-
 for (i in 1:10){
   train <- city[-folds[[i]],] 
   test <- city[folds[[i]],] 
@@ -38,8 +41,79 @@ for (i in 1:10){
   DT_result[nrow(DT_result)+1, ] = c(CM$overall['Accuracy'], CM$byClass['Precision'],
                                      CM$byClass['Recall'], CM$byClass['F1'])
 }
-  
-
-# 3. Conclude results ----
-colMeans(DT_result)
+colMeans(DT_Result)
 summary(prune.trees)$used
+
+
+# 4 Random forest ----
+# Parameters Tuning
+#   - ntree = no.of trees (row samples size = nrow(train))
+#   - mtry = no. features that samples
+#   - nodesize  = min size of terminal nodes
+
+## 4.1 Tune ntree (manual) ----
+for (n in seq(100, 500, 100)){
+  RF_result <- data.frame(matrix(ncol=4, nrow=0))
+  colnames(RF_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
+  for (i in 1:10){
+    train <- city[-folds[[i]],] 
+    test <- city[folds[[i]],] 
+    
+    RF <- randomForest(is_canceled~., data=train, ntree=n, mtry=sqrt(33), nodesize=1)
+    
+    RF.pred <- predict(RF, test, type='response')
+    RF_CM <- confusionMatrix(RF.pred, test$is_canceled)
+    RF_result[nrow(RF_result)+1, ] = c(RF_CM$overall['Accuracy'], RF_CM$byClass['Precision'],
+                                       RF_CM$byClass['Recall'], RF_CM$byClass['F1'])
+  }
+  print(n)
+  print(colMeans(RF_result))
+}
+
+## 4.2 Tune mtry with tuneRF function ----
+RF_result <- data.frame(matrix(ncol=4, nrow=0))
+colnames(RF_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
+for (i in 1:10){
+  train <- city[-folds[[i]],] 
+  test <- city[folds[[i]],] 
+  
+  tune_mtry <- tuneRF(train[,-1], train[,1], stepFactor = 2, ntreeTry=50, plot=TRUE, trace=FALSE)
+  print(tune_mtry)
+}
+
+## 4.3 Tune nodesize (manual) ----
+for (n in seq(3, 10, 2)){
+  RF_result <- data.frame(matrix(ncol=4, nrow=0))
+  colnames(RF_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
+  for (i in 1:10){
+    train <- city[-folds[[i]],] 
+    test <- city[folds[[i]],] 
+    
+    RF <- randomForest(is_canceled~., data=train, ntree=100, mtry=20, nodesize=n)
+    
+    RF.pred <- predict(RF, test, type='response')
+    RF_CM <- confusionMatrix(RF.pred, test$is_canceled)
+    RF_result[nrow(RF_result)+1, ] = c(RF_CM$overall['Accuracy'], RF_CM$byClass['Precision'],
+                                       RF_CM$byClass['Recall'], RF_CM$byClass['F1'])
+  }
+  print(n)
+  print(colMeans(RF_result))
+}
+
+## 4.4 Final result ----
+RF_result <- data.frame(matrix(ncol=4, nrow=0))
+colnames(RF_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
+for (i in 1:10){
+  train <- city[-folds[[i]],] 
+  test <- city[folds[[i]],] 
+  
+  RF <- randomForest(is_canceled~., data=train, ntree=100, mtry=20, nodesize=3)
+  
+  RF.pred <- predict(RF, test, type='response')
+  RF_CM <- confusionMatrix(RF.pred, test$is_canceled)
+  RF_result[nrow(RF_result)+1, ] = c(RF_CM$overall['Accuracy'], RF_CM$byClass['Precision'],
+                                     RF_CM$byClass['Recall'], RF_CM$byClass['F1'])
+}
+
+colMeans(RF_result)
+importance(RF)
--- hotel_resort.R
+++ hotel_resort.R
@@ -1,6 +1,7 @@
 source('preprocess.R')
 library(tree)
 library(tidyverse) 
+library(randomForest)
 
 
 # 1. Import data ----
@@ -10,36 +11,108 @@ sapply(resort, class)
 summary(resort$is_canceled)
 
 
-# 2. Do classification ----
+# 2. Split cross-validation ----
 set.seed(1)
 folds <- createFolds(resort$is_canceled, k=10)
+
+
+# 3 Decision Tree ----
 DT_result <- data.frame(matrix(ncol=4, nrow=0))
 colnames(DT_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
-
 for (i in 1:10){
   train <- resort[-folds[[i]],] 
   test <- resort[folds[[i]],] 
   
-  ## 2.1 Decision Tree ----
   trees <- tree(is_canceled~., data=train, split=c("deviance", "gini"))
   # summary(trees)
   # plot(trees)
   # text(trees, pretty=0)
-  
+
   cv.trees <- cv.tree(trees, FUN=prune.misclass, K=10)
   prune.trees <- prune.misclass(trees, best=cv.trees$size[which.min(cv.trees$dev)])
   # plot(cv.trees)
   # plot(prune.trees)
   # text(prune.trees, pretty=0)
   # summary(prune.trees)$used
-  
+
   tree.pred <- predict(prune.trees, test, type='class')
-  CM <- confusionMatrix(tree.pred, test$is_canceled)
-  DT_result[nrow(DT_result)+1, ] = c(CM$overall['Accuracy'], CM$byClass['Precision'],
-                                     CM$byClass['Recall'], CM$byClass['F1'])
+  DT_CM <- confusionMatrix(tree.pred, test$is_canceled)
+  DT_result[nrow(DT_result)+1, ] = c(DT_CM$overall['Accuracy'], DT_CM$byClass['Precision'],
+                                     DT_CM$byClass['Recall'], DT_CM$byClass['F1'])
+}
+colMeans(DT_Result)
+summary(prune.trees)$used
+
+
+# 4 Random forest ----
+# Parameters Tuning
+#   - ntree = no.of trees (row samples size = nrow(train))
+#   - mtry = no. features that samples
+#   - nodesize  = min size of terminal nodes
+
+## 4.1 Tune ntree (manual) ----
+for (n in seq(100, 500, 100)){
+  RF_result <- data.frame(matrix(ncol=4, nrow=0))
+  colnames(RF_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
+  for (i in 1:10){
+    train <- resort[-folds[[i]],] 
+    test <- resort[folds[[i]],] 
+  
+    RF <- randomForest(is_canceled~., data=train, ntree=n, mtry=sqrt(33), nodesize=1)
+    
+    RF.pred <- predict(RF, test, type='response')
+    RF_CM <- confusionMatrix(RF.pred, test$is_canceled)
+    RF_result[nrow(RF_result)+1, ] = c(RF_CM$overall['Accuracy'], RF_CM$byClass['Precision'],
+                                       RF_CM$byClass['Recall'], RF_CM$byClass['F1'])
+  }
+  print(n)
+  print(colMeans(RF_result))
 }
 
+## 4.2 Tune mtry with tuneRF function ----
+RF_result <- data.frame(matrix(ncol=4, nrow=0))
+colnames(RF_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
+for (i in 1:10){
+  train <- resort[-folds[[i]],] 
+  test <- resort[folds[[i]],] 
+  
+  tune_mtry <- tuneRF(train[,-1], train[,1], stepFactor = 2, ntreeTry=50, plot=TRUE, trace=FALSE)
+  print(tune_mtry)
+}
 
-# 3. Conclude results ----
-colMeans(DT_result)
-summary(prune.trees)$used
+## 4.3 Tune nodesize (manual) ----
+for (n in seq(1, 3, 1)){
+  RF_result <- data.frame(matrix(ncol=4, nrow=0))
+  colnames(RF_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
+  for (i in 1:10){
+    train <- resort[-folds[[i]],] 
+    test <- resort[folds[[i]],] 
+    
+    RF <- randomForest(is_canceled~., data=train, ntree=100, mtry=10, nodesize=n)
+    
+    RF.pred <- predict(RF, test, type='response')
+    RF_CM <- confusionMatrix(RF.pred, test$is_canceled)
+    RF_result[nrow(RF_result)+1, ] = c(RF_CM$overall['Accuracy'], RF_CM$byClass['Precision'],
+                                       RF_CM$byClass['Recall'], RF_CM$byClass['F1'])
+  }
+  print(n)
+  print(colMeans(RF_result))
+}
+
+## 4.4 Final result ----
+RF_result <- data.frame(matrix(ncol=4, nrow=0))
+colnames(RF_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
+for (i in 1:10){
+  train <- resort[-folds[[i]],] 
+  test <- resort[folds[[i]],] 
+  
+  RF <- randomForest(is_canceled~., data=train, ntree=100, mtry=10, nodesize=1)
+  
+  RF.pred <- predict(RF, test, type='response')
+  RF_CM <- confusionMatrix(RF.pred, test$is_canceled)
+  RF_result[nrow(RF_result)+1, ] = c(RF_CM$overall['Accuracy'], RF_CM$byClass['Precision'],
+                                     RF_CM$byClass['Recall'], RF_CM$byClass['F1'])
+}
+
+colMeans(RF_result)
+importance(RF)
