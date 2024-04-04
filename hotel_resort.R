78bf1b973e6a63043caaf38333cf3b661f0b25c4
natthaphon-wr <natthaphon.win@gmail.com>
2024-04-04 20:32:05 +0700

    gbm for resort hotel

--- hotel_resort.R
+++ hotel_resort.R
@@ -2,6 +2,7 @@ source('preprocess.R')
 library(tree)
 library(tidyverse) 
 library(randomForest)
+library(gbm)
 
 
 # 1. Import data ----
@@ -40,7 +41,7 @@ for (i in 1:10){
   DT_result[nrow(DT_result)+1, ] = c(DT_CM$overall['Accuracy'], DT_CM$byClass['Precision'],
                                      DT_CM$byClass['Recall'], DT_CM$byClass['F1'])
 }
-colMeans(DT_Result)
+clcolMeans(DT_Result)
 summary(prune.trees)$used
 
 
@@ -116,3 +117,162 @@ for (i in 1:10){
 
 colMeans(RF_result)
 importance(RF)
+
+
+# 5. Gradient Boosting ----
+# Parameters Tuning
+#   - classification threshold 
+#   - n.trees = total no. of tree
+#   - shrinkage = learning rate
+#   - n.minobsinnode = min no. of observations in terminal node
+#   - bag.fraction = fraction of training set to be selected to build next tree
+
+resort_GBM <- resort
+resort_GBM$is_canceled <- unclass(resort_GBM$is_canceled)%%2 #Change variable type
+
+## 5.1 Tuning classify threshold -----
+GBM_tuning0 <- data.frame(matrix(ncol=5, nrow=0))
+colnames(GBM_tuning0) = c('threshold', 'Accuracy', 'Precision', 'Recall', 'F1')
+for (j in seq(0.35, 0.45, 0.01)){
+  GBM_result <- data.frame(matrix(ncol=4, nrow=0))
+  colnames(GBM_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
+  for (i in 1:10){
+    train <- resort_GBM[-folds[[i]],] 
+    test <- resort_GBM[folds[[i]],] 
+    
+    GBM <- gbm(is_canceled~., data=train, distribution="bernoulli", n.trees=500, n.minobsinnode=80, 
+               shrinkage=0.02, bag.fraction=0.4, verbose=FALSE)
+    # summary(GBM) #compute relative inference of each variable
+  
+    GBM.pred <- predict.gbm(GBM, test, type='response', verbose=FALSE)
+    GBM.pred[GBM.pred < j] <- 0
+    GBM.pred[GBM.pred >= j] <- 1
+    GBM.pred <- factor(GBM.pred, levels = c(1,0))
+    
+    test$is_canceled <- factor(test$is_canceled, levels = c(1,0))
+    GBM_CM <- confusionMatrix(GBM.pred, test$is_canceled)
+    GBM_result[nrow(GBM_result)+1, ] = c(GBM_CM$overall['Accuracy'], GBM_CM$byClass['Precision'],
+                                         GBM_CM$byClass['Recall'], GBM_CM$byClass['F1'])
+  }
+  # print(j)
+  # print(colMeans(GBM_result))
+  GBM_tuning0[nrow(GBM_tuning0)+1, ] = c(j, colMeans(GBM_result)[1], colMeans(GBM_result)[2],
+                                       colMeans(GBM_result)[3], colMeans(GBM_result)[4])
+}
+GBM_tuning0
+
+## 5.2 Tuning n.trees and shrinkage -----
+GBM_tuning <- data.frame(matrix(ncol=6, nrow=0))
+colnames(GBM_tuning) = c('n.trees', 'shrinkage', 'Accuracy', 'Precision', 'Recall', 'F1')
+for (n in seq(500, 1000, 100)){
+  for (rl in seq(0.01, 0.03, 0.01)){
+    GBM_result <- data.frame(matrix(ncol=4, nrow=0))
+    colnames(GBM_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
+    for (i in 1:10){
+      train <- resort_GBM[-folds[[i]],] 
+      test <- resort_GBM[folds[[i]],] 
+      
+      GBM <- gbm(is_canceled~., data=train, distribution="bernoulli", n.trees=n, n.minobsinnode=10, 
+                 shrinkage=rl, bag.fraction=0.5, verbose=FALSE)
+      # summary(GBM) #compute relative inference of each variable
+      
+      GBM.pred <- predict.gbm(GBM, test, type='response', verbose=FALSE)
+      GBM.pred[GBM.pred < 0.35] <- 0
+      GBM.pred[GBM.pred >= 0.35] <- 1
+      GBM.pred <- factor(GBM.pred, levels = c(1,0))
+      
+      test$is_canceled <- factor(test$is_canceled, levels = c(1,0))
+      GBM_CM <- confusionMatrix(GBM.pred, test$is_canceled)
+      GBM_result[nrow(GBM_result)+1, ] = c(GBM_CM$overall['Accuracy'], GBM_CM$byClass['Precision'],
+                                           GBM_CM$byClass['Recall'], GBM_CM$byClass['F1'])
+    }
+    GBM_tuning[nrow(GBM_tuning)+1, ] = c(n, rl, colMeans(GBM_result)[1], colMeans(GBM_result)[2],
+                                         colMeans(GBM_result)[3], colMeans(GBM_result)[4])
+  }
+}
+GBM_tuning
+
+## 5.3 Tuning n.minobsinnode -----
+GBM_tuning2 <- data.frame(matrix(ncol=5, nrow=0))
+colnames(GBM_tuning2) = c('n.minobsinnode', 'Accuracy', 'Precision', 'Recall', 'F1')
+for (n in seq(10, 100, 10)){
+  GBM_result <- data.frame(matrix(ncol=4, nrow=0))
+  colnames(GBM_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
+  for (i in 1:10){
+    train <- resort_GBM[-folds[[i]],] 
+    test <- resort_GBM[folds[[i]],] 
+    
+    GBM <- gbm(is_canceled~., data=train, distribution="bernoulli", n.trees=500, shrinkage=0.02, 
+               n.minobsinnode=n, bag.fraction=0.5, verbose=FALSE)
+    # summary(GBM) #compute relative inference of each variable
+    
+    GBM.pred <- predict.gbm(GBM, test, type='response', verbose=FALSE)
+    GBM.pred[GBM.pred < 0.35] <- 0
+    GBM.pred[GBM.pred >= 0.35] <- 1
+    GBM.pred <- factor(GBM.pred, levels = c(1,0))
+    
+    test$is_canceled <- factor(test$is_canceled, levels = c(1,0))
+    GBM_CM <- confusionMatrix(GBM.pred, test$is_canceled)
+    GBM_result[nrow(GBM_result)+1, ] = c(GBM_CM$overall['Accuracy'], GBM_CM$byClass['Precision'],
+                                         GBM_CM$byClass['Recall'], GBM_CM$byClass['F1'])
+  }
+  GBM_tuning2[nrow(GBM_tuning2)+1, ] = c(n, colMeans(GBM_result)[1], colMeans(GBM_result)[2],
+                                       colMeans(GBM_result)[3], colMeans(GBM_result)[4])
+  # print(n)
+  # print(colMeans(GBM_result))
+}
+GBM_tuning2
+
+
+## 5.4 Tuning bag.fraction ----
+GBM_tuning3 <- data.frame(matrix(ncol=5, nrow=0))
+colnames(GBM_tuning3) = c('bag.fraction', 'Accuracy', 'Precision', 'Recall', 'F1')
+for (n in seq(0.3, 0.7, 0.1)){
+  GBM_result <- data.frame(matrix(ncol=4, nrow=0))
+  colnames(GBM_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
+  for (i in 1:10){
+    train <- resort_GBM[-folds[[i]],] 
+    test <- resort_GBM[folds[[i]],] 
+    
+    GBM <- gbm(is_canceled~., data=train, distribution="bernoulli", n.trees=500, shrinkage=0.02, 
+               n.minobsinnode=80, bag.fraction=n, verbose=FALSE)
+    # summary(GBM) #compute relative inference of each variable
+    
+    GBM.pred <- predict.gbm(GBM, test, type='response', verbose=FALSE)
+    GBM.pred[GBM.pred < 0.35] <- 0
+    GBM.pred[GBM.pred >= 0.35] <- 1
+    GBM.pred <- factor(GBM.pred, levels = c(1,0))
+    
+    test$is_canceled <- factor(test$is_canceled, levels = c(1,0))
+    GBM_CM <- confusionMatrix(GBM.pred, test$is_canceled)
+    GBM_result[nrow(GBM_result)+1, ] = c(GBM_CM$overall['Accuracy'], GBM_CM$byClass['Precision'],
+                                         GBM_CM$byClass['Recall'], GBM_CM$byClass['F1'])
+  }
+  GBM_tuning3[nrow(GBM_tuning3)+1, ] = c(n, colMeans(GBM_result)[1], colMeans(GBM_result)[2],
+                                         colMeans(GBM_result)[3], colMeans(GBM_result)[4])
+}
+GBM_tuning3
+
+## 5.5 Final result ----
+GBM_result <- data.frame(matrix(ncol=4, nrow=0))
+colnames(GBM_result) = c('Accuracy', 'Precision', 'Recall', 'F1')
+for (i in 1:10){
+  train <- resort_GBM[-folds[[i]],] 
+  test <- resort_GBM[folds[[i]],] 
+  
+  GBM <- gbm(is_canceled~., data=train, distribution="bernoulli", n.trees=500, shrinkage=0.02, 
+             n.minobsinnode=80, bag.fraction=0.4, verbose=FALSE)
+  # summary(GBM) #compute relative inference of each variable
+  
+  GBM.pred <- predict.gbm(GBM, test, type='response', verbose=FALSE)
+  GBM.pred[GBM.pred < 0.35] <- 0
+  GBM.pred[GBM.pred >= 0.35] <- 1
+  GBM.pred <- factor(GBM.pred, levels = c(1,0))
+  
+  test$is_canceled <- factor(test$is_canceled, levels = c(1,0))
+  GBM_CM <- confusionMatrix(GBM.pred, test$is_canceled)
+  GBM_result[nrow(GBM_result)+1, ] = c(GBM_CM$overall['Accuracy'], GBM_CM$byClass['Precision'],
+                                       GBM_CM$byClass['Recall'], GBM_CM$byClass['F1'])
+}
+colMeans(GBM_result)
+#summary(GBM)
